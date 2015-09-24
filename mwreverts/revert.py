3650d11f1ba3761b003b2e89dcef2c905d643634
halfak <aaron.halfaker@gmail.com>
2015-09-24 11:26:45 -0500

    Converts Revert into a JSONable object

--- mwreverts/revert.py
+++ mwreverts/revert.py
@@ -1,17 +1,42 @@
-from collections import namedtuple
+import jsonable
 
-Revert = namedtuple("Revert", ['reverting', 'reverteds', 'reverted_to'])
-"""
-Represents a revert event.  This class behaves like
-:class:`collections.namedtuple`.  Note that the datatypes of `reverting`,
-`reverteds` and `reverted_to` is not specified since those types will depend
-on the revision data provided during revert detection.
 
-:Attributes:
-    **reverting**
-        The reverting revision data : `mixed`
-    **reverteds**
-        The reverted revision data (ordered chronologically) : list( `mixed` )
-    **reverted_to**
-        The reverted-to revision data : `mixed`
-"""
+class Revert(jsonable.Type):
+    """
+    Represents a revert event.  This class behaves like
+    :class:`collections.namedtuple`.  Note that the datatypes of `reverting`,
+    `reverteds` and `reverted_to` is not specified since those types will
+    depend on the revision data provided during revert detection.
+
+    :Attributes:
+        **reverting**
+            The reverting revision data : `mixed`
+        **reverteds**
+            The reverted revision data (ordered chronologically) :
+            list( `mixed` )
+        **reverted_to**
+            The reverted-to revision data : `mixed`
+    """
+    __slots__ = ('reverting', 'reverteds', 'reverted_to')
+
+    def initialize(self, reverting=None, reverteds=None, reverted_to=None):
+        self.reverting = reverting
+        self.reverteds = list(reverteds or [])
+        self.reverted_to = reverted_to
+
+    def __iter__(self):
+        yield self.reverting
+        yield self.reverteds
+        yield self.reverted_to
+
+    def __eq__(self, other):
+        if isinstance(other, tuple):
+            return tuple(self) == other
+
+    def __getitem__(self, index):
+        if index == 0:
+            return self.reverting
+        elif index == 1:
+            return self.reverteds
+        else:
+            return self.reverted_to

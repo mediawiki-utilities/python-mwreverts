4a236d25970ab58d3494e083c252d6ab429a17fd
halfak <aaron.halfaker@gmail.com>
2016-12-14 14:26:27 -0600

    Adds db.check_archive() method

--- demo_db_check.py
+++ demo_db_check.py
@@ -10,9 +10,15 @@ def format_revert(revert):
     if revert is None:
         return ""
     else:
-        return " ".join([str(revert.reverting.rev_id),
-                         str([r.rev_id for r in revert.reverteds]),
-                         str(revert.reverted_to.rev_id)])
+        return " ".join([str(get_rev_id(revert.reverting)),
+                         str([get_rev_id(r) for r in revert.reverteds]),
+                         str(get_rev_id(revert.reverted_to))])
+
+def get_rev_id(row):
+    if hasattr(row, 'rev_id'):
+        return row.rev_id
+    else:
+        return row.ar_rev_id
 
 reverting, reverted, reverted_to = mwreverts.db.check(enwiki, 679778587)
 print("reverting:", format_revert(reverting))
@@ -31,6 +37,33 @@ print("---------------")
 
 reverting, reverted, reverted_to = \
     mwreverts.db.check(enwiki, reverting.reverted_to.rev_id)
+print("reverting:", format_revert(reverting))
+print("reverted:", format_revert(reverted))
+print("reverted_to:", format_revert(reverted_to))
+
+print("---------------")
+print("Checking an archived edit")
+print("---------------")
+
+
+reverting, reverted, reverted_to = \
+    mwreverts.db.check_archive(enwiki, 587400097)
+print("reverting:", format_revert(reverting))
+print("reverted:", format_revert(reverted))
+print("reverted_to:", format_revert(reverted_to))
+
+print("---------------")
+
+reverting, reverted, reverted_to = \
+    mwreverts.db.check_archive(enwiki, reverted.reverting.ar_rev_id)
+print("reverting:", format_revert(reverting))
+print("reverted:", format_revert(reverted))
+print("reverted_to:", format_revert(reverted_to))
+
+print("---------------")
+
+reverting, reverted, reverted_to = \
+    mwreverts.db.check_archive(enwiki, reverting.reverted_to.ar_rev_id)
 print("reverting:", format_revert(reverting))
 print("reverted:", format_revert(reverted))
 print("reverted_to:", format_revert(reverted_to))
